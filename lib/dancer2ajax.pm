7dafb6cc525fd545a86fa2adfdaa5792f6c20224
Jason Lewis <jason@dickson.st>
2016-03-01 12:34:19 +1100

    try Dancer2::Plugin::DBIC

--- lib/dancer2ajax.pm
+++ lib/dancer2ajax.pm
@@ -3,9 +3,41 @@ use Dancer2;
 use Dancer2::Plugin::Ajax;
 use Dancer2::Plugin::Database;
 use DBI;
+use Dancer2::Plugin::DBIC;
+#use dancer2ajax::Schema;
+use Data::Dumper;
+
 #my $dbh = DBI->connect("dbi:SQLite:dbname=database/Chinook_Sqlite.sqlite","","");
 
 our $VERSION = '0.1';
+
+ajax '/album.json' => sub {
+  my $rs  = resultset('Album')->find('14');
+
+#  $rs->result_class('DBIx::Class::ResultClass::HashRefInflator');
+  #print Dumper($rs);
+  to_json {data => [$rs]};
+};
+
+# ajax '/artist.json' => sub {
+#   my $artist = schema->resultset('Artist')->search(
+#     {name => {like => '%Red%'}},
+#   );
+#   $artist->result_class('DBIx::Class::ResultClass::HashRefInflator');
+#   to_json {data => [$artist->all] };
+# };
+
+# ajax '/artistalbum.json' => sub {
+#   my $artist = schema->resultset('Artist')->search(
+#     {name => {like => '%Red%'}},
+#     {join => 'albums',
+#      '+select' => ['albums.Title'],
+#      order_by => ['Name'],
+#     },
+#   );
+#   $artist->result_class('DBIx::Class::ResultClass::HashRefInflator');
+#   to_json {data => [$artist->all] };
+# };
 
 ajax '/time' => sub {
   my $timestr = localtime;
