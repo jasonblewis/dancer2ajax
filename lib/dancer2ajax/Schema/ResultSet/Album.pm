7fffed7c26612a55e69751159177bfeca55127e0
Jason Lewis <jason@dickson.st>
2016-03-02 20:43:19 +1100

    album resultset not required

--- lib/dancer2ajax/Schema/ResultSet/Album.pm
+++
@@ -1,8 +0,0 @@
-use utf8;
-package dancer2ajax::Schema::ResultSet::Album;
-
-use base 'dancer2ajax::Schema::ResultSet';
-
-# see https://metacpan.org/pod/distribution/DBIx-Class/lib/DBIx/Class/Manual/Cookbook.pod#Predefined-searches
-
-1;
